107e7c44fea4ffb27771128037d8cfdf22bf8bd1
Abbas4tech <shaikhabbas54@gmail.com>
2022-02-04 19:07:44 +0530

    submit form data and clear inputs

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -4,43 +4,64 @@ import "./NewExpense.css";
 const ExpenseForm = () => {
   //================Individual UseStates for Each Input================
 
-  //   const [enteredTitle, setEnteredTitle] = useState("");
-  //   const [enteredPrice, setEnteredPrice] = useState("");
-  //   const [enteredDate, setEnteredDate] = useState("");
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredPrice, setEnteredPrice] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
-  //   const titleChangeHandler = (event) => setEnteredTitle(event.target.value);
-  //   const priceChangeHandler = (event) => setEnteredPrice(event.target.value);
-  //   const dateChangeHandler = (event) => setEnteredDate(event.target.value);
+  const titleChangeHandler = (event) => setEnteredTitle(event.target.value);
+  const priceChangeHandler = (event) => setEnteredPrice(event.target.value);
+  const dateChangeHandler = (event) => setEnteredDate(event.target.value);
 
   //=====================Single UseState for Each Input=====================
 
-  const [enteredInputs, setEnteredInputs] = useState({
-    enteredTitle: "",
-    enteredPrice: "",
-    enteredDate: "",
-  });
+  //   const [enteredInputs, setEnteredInputs] = useState({
+  //     enteredTitle: "",
+  //     enteredPrice: "",
+  //     enteredDate: "",
+  //   });
 
-  const titleChangeHandler = (event) =>
-    setEnteredInputs({
-      ...enteredInputs,
-      enteredTitle: event.target.value,
-    });
-  const priceChangeHandler = (event) =>
-    setEnteredInputs({
-      ...enteredInputs,
-      enteredPrice: event.target.value,
-    });
-  const dateChangeHandler = (event) =>
-    setEnteredInputs({
-      ...enteredInputs,
-      enteredDate: event.target.value,
-    });
+  //   const titleChangeHandler = (event) =>
+  //     setEnteredInputs({
+  //       ...enteredInputs,
+  //       enteredTitle: event.target.value,
+  //     });
+  //   const priceChangeHandler = (event) =>
+  //     setEnteredInputs({
+  //       ...enteredInputs,
+  //       enteredPrice: event.target.value,
+  //     });
+  //   const dateChangeHandler = (event) =>
+  //     setEnteredInputs({
+  //       ...enteredInputs,
+  //       enteredDate: event.target.value,
+  //     });
+
+  //===================================================================
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      price: enteredPrice,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredPrice("");
+    setEnteredDate("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            onChange={titleChangeHandler}
+            value={enteredTitle}
+          />
         </div>
         <div className="new-expense__control">
           <label>Price</label>
@@ -48,6 +69,7 @@ const ExpenseForm = () => {
             type="number"
             step="0.01"
             min="0.01"
+            value={enteredPrice}
             onChange={priceChangeHandler}
           />
         </div>
@@ -57,6 +79,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
